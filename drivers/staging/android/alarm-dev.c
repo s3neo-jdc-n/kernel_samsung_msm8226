c3e5334ec203470f10f670ac1fb98787eb715361
John Stultz <john.stultz@linaro.org>
2013-01-11 15:46:24 -0800

    staging: alarm-dev: Implement compat_ioctl support
    Implement compat_ioctl support for the alarm-dev ioctl.
    
    Change-Id: Ib33c67f9d25fd4d6401388b9c7dc012a8f3a9bd6
    Cc: Serban Constantinescu <serban.constantinescu@arm.com>
    Cc: Arve Hjønnevåg <arve@android.com>
    Cc: Colin Cross <ccross@google.com>
    Cc: Android Kernel Team <kernel-team@android.com>
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/staging/android/alarm-dev.c
+++ drivers/staging/android/alarm-dev.c
@@ -397,6 +397,38 @@ static long alarm_compat_ioctl(struct file *file, unsigned int cmd,
 
 	return 0;
 }
+
+#ifdef CONFIG_COMPAT
+static long alarm_compat_ioctl(struct file *file, unsigned int cmd,
+							unsigned long arg)
+{
+
+	struct timespec ts;
+	int rv;
+
+	switch (ANDROID_ALARM_BASE_CMD(cmd)) {
+	case ANDROID_ALARM_SET_AND_WAIT_COMPAT(0):
+	case ANDROID_ALARM_SET_COMPAT(0):
+	case ANDROID_ALARM_SET_RTC_COMPAT:
+		if (compat_get_timespec(&ts, (void __user *)arg))
+			return -EFAULT;
+		/* fall through */
+	case ANDROID_ALARM_GET_TIME_COMPAT(0):
+		cmd = ANDROID_ALARM_COMPAT_TO_NORM(cmd);
+		break;
+	}
+
+	rv = alarm_do_ioctl(file, cmd, &ts);
+
+	switch (ANDROID_ALARM_BASE_CMD(cmd)) {
+	case ANDROID_ALARM_GET_TIME(0): /* NOTE: we modified cmd above */
+		if (compat_put_timespec(&ts, (void __user *)arg))
+			return -EFAULT;
+		break;
+	}
+
+	return rv;
+}
 #endif
 
 static int alarm_open(struct inode *inode, struct file *file)

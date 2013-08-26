63d2c980596b2b5d1f7e21b3c16c5a1ac3052d0b
Stratos Karafotis <stratosk@semaphore.gr>
2013-08-26 21:42:21 +0300

    cpufreq: governor: Fix typos in comments
     - 'Governer' should be 'Governor'.
     - 'S' is used for Siemens (electrical conductance) in SI units,
       so use small 's' for seconds.
    
    Signed-off-by: Stratos Karafotis <stratosk@semaphore.gr>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpufreq/cpufreq_governor.h
+++ drivers/cpufreq/cpufreq_governor.h
@@ -26,11 +26,11 @@
 /*
  * The polling frequency depends on the capability of the processor. Default
  * polling frequency is 1000 times the transition latency of the processor. The
- * governor will work on any processor with transition latency <= 10mS, using
+ * governor will work on any processor with transition latency <= 10ms, using
  * appropriate sampling rate.
  *
- * For CPUs with transition latency > 10mS (mostly drivers with CPUFREQ_ETERNAL)
- * this governor will not work. All times here are in uS.
+ * For CPUs with transition latency > 10ms (mostly drivers with CPUFREQ_ETERNAL)
+ * this governor will not work. All times here are in us (micro seconds).
  */
 #define MIN_SAMPLING_RATE_RATIO			(2)
 #define LATENCY_MULTIPLIER			(1000)
@@ -185,7 +185,7 @@ struct nm_cpu_dbs_info_s {
 	struct cpufreq_frequency_table *freq_table;
 };
 
-/* Per policy Governers sysfs tunables */
+/* Per policy Governors sysfs tunables */
 struct od_dbs_tuners {
 	unsigned int ignore_nice;
 	unsigned int sampling_rate;
@@ -237,7 +237,7 @@ struct nm_dbs_tuners {
 	int freq_step_dec_at_max_freq;
 };
 
-/* Common Governer data across policies */
+/* Common Governor data across policies */
 struct dbs_data;
 struct common_dbs_data {
 	/* Common across governors */
@@ -264,7 +264,7 @@ struct common_dbs_data {
 	void *gov_ops;
 };
 
-/* Governer Per policy data */
+/* Governor Per policy data */
 struct dbs_data {
 	struct common_dbs_data *cdata;
 	unsigned int min_sampling_rate;

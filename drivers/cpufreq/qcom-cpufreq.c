8f7a2dd23edea5f04b8befc4d18f687ebfd2f9d0
alucard24 <dmbaoh2@gmail.com>
2016-02-25 00:12:36 +0100

    qcom-cpufreq: we don't need to register an hotplug notifier is "is_clk" var is false!

--- drivers/cpufreq/qcom-cpufreq.c
+++ drivers/cpufreq/qcom-cpufreq.c
@@ -647,7 +647,8 @@ static int __init msm_cpufreq_register(void)
 
 	platform_driver_probe(&msm_cpufreq_plat_driver, msm_cpufreq_probe);
 	msm_cpufreq_wq = alloc_workqueue("msm-cpufreq", WQ_HIGHPRI, 0);
-	register_hotcpu_notifier(&msm_cpufreq_cpu_notifier);
+	if (is_clk)
+		register_hotcpu_notifier(&msm_cpufreq_cpu_notifier);
 	register_pm_notifier(&msm_cpufreq_pm_notifier);
 	return cpufreq_register_driver(&msm_cpufreq_driver);
 }

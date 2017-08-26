7ca6ba93ff160edfac3749d4d85e69eb8f733f8e
Kevin F. Haggerty <haggertk@lineageos.org>
2017-08-26 12:24:53 -0600

    thermal: msm_thermal: Migrate to alarmtimer interface
    Change-Id: Ibb0a4646b7caee27cbb9c19224ba415c4128eceb
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -30,11 +30,8 @@
 #include <linux/of.h>
 #include <linux/sysfs.h>
 #include <linux/types.h>
-<<<<<<< HEAD
-#include <linux/android_alarm.h>
-=======
 #include <linux/io.h>
->>>>>>> 5f5a32f0ea24... msm: thermal: Remove alarm support in KTM
+#include <linux/hrtimer.h>
 #include <linux/thermal.h>
 #include <mach/rpm-regulator.h>
 #include <mach/rpm-regulator-smd.h>
@@ -51,7 +48,11 @@ static struct delayed_work temp_log_work;
 static bool core_control_enabled;
 static uint32_t cpus_offlined;
 static DEFINE_MUTEX(core_control_mutex);
+static uint32_t wakeup_ms;
+static struct hrtimer thermal_rtc_hrtimer;
+static struct kobject *tt_kobj;
 static struct kobject *cc_kobj;
+static struct work_struct timer_work;
 static struct task_struct *hotplug_task;
 static struct task_struct *freq_mitigation_task;
 static struct completion hotplug_notify_complete;
@@ -1233,6 +1234,41 @@ static struct notifier_block __refdata msm_thermal_cpu_notifier = {
 	.notifier_call = msm_thermal_cpu_callback,
 };
 
+static void thermal_rtc_setup(void)
+{
+	ktime_t wakeup_time;
+	ktime_t curr_time;
+
+	curr_time = ktime_get_boottime();
+	wakeup_time = ktime_add_us(curr_time,
+			(wakeup_ms * USEC_PER_MSEC));
+	hrtimer_start_range_ns(&thermal_rtc_hrtimer, wakeup_time,
+			ULONG_MAX, HRTIMER_MODE_ABS);
+	pr_debug("%s: Current Time: %ld %ld, Alarm set to: %ld %ld\n",
+			KBUILD_MODNAME,
+			ktime_to_timeval(curr_time).tv_sec,
+			ktime_to_timeval(curr_time).tv_usec,
+			ktime_to_timeval(wakeup_time).tv_sec,
+			ktime_to_timeval(wakeup_time).tv_usec);
+
+}
+
+static void timer_work_fn(struct work_struct *work)
+{
+	sysfs_notify(tt_kobj, NULL, "wakeup_ms");
+}
+
+enum hrtimer_restart thermal_rtc_callback(struct hrtimer *timer)
+{
+	struct timespec ts;
+	get_monotonic_boottime(&ts);
+	schedule_work(&timer_work);
+	pr_debug("%s: Time on alarm expiry: %ld %ld\n", KBUILD_MODNAME,
+			ts.tv_sec, ts.tv_nsec / 1000);
+
+	return HRTIMER_NORESTART;
+}
+
 static int hotplug_notify(enum thermal_trip_type type, int temp, void *data)
 {
 	struct cpu_info *cpu_node = (struct cpu_info *)data;
@@ -1660,6 +1696,53 @@ static __refdata struct attribute *cc_attrs[] = {
 static __refdata struct attribute_group cc_attr_group = {
 	.attrs = cc_attrs,
 };
+
+static ssize_t show_wakeup_ms(struct kobject *kobj,
+		struct kobj_attribute *attr, char *buf)
+{
+	return snprintf(buf, PAGE_SIZE, "%d\n", wakeup_ms);
+}
+
+static ssize_t store_wakeup_ms(struct kobject *kobj,
+		struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	int ret;
+	ret = kstrtouint(buf, 10, &wakeup_ms);
+
+	if (ret) {
+		pr_err("%s: Trying to set invalid wakeup timer\n",
+				KBUILD_MODNAME);
+		return ret;
+	}
+
+	if (wakeup_ms > 0) {
+		thermal_rtc_setup();
+		pr_debug("%s: Timer started for %ums\n", KBUILD_MODNAME,
+				wakeup_ms);
+	} else {
+		ret = hrtimer_cancel(&thermal_rtc_hrtimer);
+		if (ret)
+			pr_debug("%s: Timer canceled\n", KBUILD_MODNAME);
+		else
+			pr_debug("%s: No active timer present to cancel\n",
+					KBUILD_MODNAME);
+
+	}
+	return count;
+}
+
+static __refdata struct kobj_attribute timer_attr =
+__ATTR(wakeup_ms, 0644, show_wakeup_ms, store_wakeup_ms);
+
+static __refdata struct attribute *tt_attrs[] = {
+	&timer_attr.attr,
+	NULL,
+};
+
+static __refdata struct attribute_group tt_attr_group = {
+	.attrs = tt_attrs,
+};
+
 static __init int msm_thermal_add_cc_nodes(void)
 {
 	struct kobject *module_kobj = NULL;
@@ -1695,34 +1778,42 @@ done_cc_nodes:
 	return ret;
 }
 
-int msm_thermal_pre_init(void)
+static __init int msm_thermal_add_timer_nodes(void)
 {
+	struct kobject *module_kobj = NULL;
 	int ret = 0;
 
-	tsens_get_max_sensor_num(&max_tsens_num);
-	if (create_sensor_id_map()) {
-		pr_err("Creating sensor id map failed\n");
-		ret = -EINVAL;
-		goto pre_init_exit;
+	module_kobj = kset_find_obj(module_kset, KBUILD_MODNAME);
+	if (!module_kobj) {
+		pr_err("%s: cannot find kobject for module\n",
+			KBUILD_MODNAME);
+		ret = -ENOENT;
+		goto failed;
 	}
 
-	if (!thresh) {
-		thresh = kzalloc(
-				sizeof(struct threshold_info) * MSM_LIST_MAX_NR,
-				GFP_KERNEL);
-		if (!thresh) {
-			pr_err("kzalloc failed\n");
-			ret = -ENOMEM;
-			goto pre_init_exit;
-		}
-		memset(thresh, 0, sizeof(struct threshold_info) *
-			MSM_LIST_MAX_NR);
+	tt_kobj = kobject_create_and_add("thermal_timer", module_kobj);
+	if (!tt_kobj) {
+		pr_err("%s: cannot create timer kobj\n",
+				KBUILD_MODNAME);
+		ret = -ENOMEM;
+		goto failed;
 	}
-pre_init_exit:
+
+	ret = sysfs_create_group(tt_kobj, &tt_attr_group);
+	if (ret) {
+		pr_err("%s: cannot create group\n", KBUILD_MODNAME);
+		goto failed;
+	}
+
+	return 0;
+
+failed:
+	if (tt_kobj)
+		kobject_del(tt_kobj);
 	return ret;
 }
 
-int msm_thermal_init(struct msm_thermal_data *pdata)
+int __devinit msm_thermal_init(struct msm_thermal_data *pdata)
 {
 	int ret = 0;
 	uint32_t cpu;
@@ -2595,7 +2686,13 @@ int __init msm_thermal_late_init(void)
 	msm_thermal_add_psm_nodes();
 	msm_thermal_add_vdd_rstr_nodes();
 	msm_thermal_add_ocr_nodes();
-	msm_thermal_add_default_temp_limit_nodes();
+	hrtimer_init(&thermal_rtc_hrtimer,
+			CLOCK_BOOTTIME,
+			HRTIMER_MODE_ABS);
+	thermal_rtc_hrtimer.function=
+			&thermal_rtc_callback;
+	INIT_WORK(&timer_work, timer_work_fn);
+	msm_thermal_add_timer_nodes();
 
 	return 0;
 }

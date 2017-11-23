4f43c2651c19ab8ee4b8f1af8622e2123d6d7762
jasio2k00 <jasio2k00@gmail.com>
2017-11-23 09:30:51 +0000

    mm: Enable KSM and and support for UKSM (patch 0.1.2.3)
    Update UKMS code to compile with Linux 3.4.113

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -151,6 +151,18 @@ enum zone_stat_item {
 #endif
 	NR_ANON_TRANSPARENT_HUGEPAGES,
 	NR_FREE_CMA_PAGES,
+
+#if defined(CONFIG_CMA_PAGE_COUNTING)
+	NR_CMA_INACTIVE_ANON,
+	NR_CMA_ACTIVE_ANON,
+	NR_CMA_INACTIVE_FILE,
+	NR_CMA_ACTIVE_FILE,
+	NR_CMA_UNEVICTABLE,
+#endif
+	NR_SWAPCACHE,
+#ifdef CONFIG_UKSM
+	NR_UKSM_ZERO_PAGES,
+#endif
 	NR_VM_ZONE_STAT_ITEMS };
 
 /*
@@ -797,7 +809,7 @@ static inline int is_normal_idx(enum zone_type idx)
 }
 
 /**
- * is_highmem - helper function to quickly check if a struct zone is a 
+ * is_highmem - helper function to quickly check if a struct zone is a
  *              highmem zone or not.  This is an attempt to keep references
  *              to ZONE_{DMA/NORMAL/HIGHMEM/etc} in general code to a minimum.
  * @zone - pointer to struct zone variable
--- mm/vmstat.c
+++ mm/vmstat.c
@@ -726,6 +726,18 @@ const char * const vmstat_text[] = {
 #endif
 	"nr_anon_transparent_hugepages",
 	"nr_free_cma",
+
+#if defined(CONFIG_CMA_PAGE_COUNTING)
+	"nr_cma_inactive_anon",
+	"nr_cma_active_anon",
+	"nr_cma_inactive_file",
+	"nr_cma_active_file",
+	"nr_cma_unevictable",
+#endif
+#ifdef CONFIG_UKSM
+	"nr_uksm_zero_pages",
+#endif
+
 	"nr_dirty_threshold",
 	"nr_dirty_background_threshold",
 
